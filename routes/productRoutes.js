40b73889f4f3b9db1e8f5b13a1755d45a0b32df8
sardar-ali <alisardar465@gmail.com>
2023-05-30 18:16:35 +0500

    added product images upload route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware")
+const { productImagesResize, blogImagesResize, uploadOptions } = require("../middlewares/uploadImagesMiddleware")
 const {
     createProduct,
     getProducts,
     getSingleProduct,
     deleteProduct,
     updateProduct,
-    addToWishlist
+    addToWishlist,
+    uploadImages
 } = require("../controllers/productController")
 const router = express.Router();
 
@@ -15,6 +17,7 @@ router.get("/getAllProducts", getProducts);
 router.get("/getSingleProduct/:id", getSingleProduct);
 router.delete("/deleteProduct/:id", authMiddleware, isAdmin, deleteProduct)
 router.put("/updateProduct/:id", authMiddleware, isAdmin, updateProduct)
+router.put("/uploads/:id", authMiddleware, isAdmin, uploadOptions.array("images", 10), productImagesResize, uploadImages)
 router.put("/addToWishlist/", authMiddleware, isAdmin, addToWishlist)
 
 module.exports = router;
